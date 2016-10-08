983fc9501b15f2a20e2663921a5a5dc1bfe73f62
weijunyi <Administrator@weijunyi-PC0.tencent.com>
2016-10-08 13:02:23 +0800

    add multiple sms demo

--- demo/SmsMultiDemo.php
+++ demo/SmsMultiDemo.php
@@ -1,3 +1,88 @@
 <?php
-// 敬请期待
+
+class SmsMultiSender {
+    var $url;
+    var $sdkappid;
+    var $appkey;
+    
+    // sdkappid 使用整数即可
+    function __construct($sdkappid, $appkey) {
+        // url 需要根据我们说明文档上适时调整
+        $this->url = "https://yun.tim.qq.com/v3/tlssmssvr/sendmultisms2";
+        $this->sdkappid = $sdkappid;
+        $this->appkey = $appkey;
+    }
+    
+    // 全部参数使用字符串即可
+    function sendSms($nationCode, $phoneNumbers, $content) {
+        if (0 == count($phoneNumbers)) {
+            return;
+        }
+
+        $randNum = rand(100000, 999999);
+        $wholeUrl = $this->url."?sdkappid=".$this->sdkappid."&random=".$randNum;
+        echo $wholeUrl."\n";
+
+        $tel = array();
+        for ($i = 0; $i < count($phoneNumbers); $i++) {
+            $telElement = new stdClass();
+            $telElement->nationcode = $nationCode;
+            $telElement->phone = $phoneNumbers[$i];
+            $tel[] = $telElement;
+        }
+
+        $jsondata = new stdClass();
+        $jsondata->tel = $tel;
+        $jsondata->type = "0";
+        $jsondata->msg = $content;
+        $jsondata->sig = $this->calculateSig($this->appkey, $phoneNumbers);
+        $jsondata->extend = "";     // 根据需要添加，一般保持默认
+        $jsondata->ext = "";        // 根据需要添加，一般保持默认
+        $curlPost = json_encode($jsondata);
+        echo $curlPost;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $wholeUrl);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $curlPost);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+        $ret = curl_exec($ch);
+
+        if($ret === false) {
+            var_dump(curl_error($ch));
+        } else {
+            $json = json_decode($ret);
+            if($json === false) {
+                var_dump($ret);
+            } else {
+                var_dump($json);
+            }
+        }
+        curl_close($ch);
+        return;
+    }
+
+    function calculateSig($appkey, $phoneNumbers) {
+        $cnt = count($phoneNumbers);
+        $string = $appkey.$phoneNumbers[0];
+        for ($i = 1; $i < $cnt; $i++) {
+            $string = $string.",".$phoneNumbers[$i];
+        }
+
+        return md5($string);
+    }
+}
+
+// 下面的 sdkappid 和 appkey 都是无法使用，开放者实际发送短信时请使用申请的 sdkappid 和 appkey
+$sender = new SmsSender(1234567890, "1234567890");
+
+// 下列手机号码均不存在，请替换成实际存在的
+$phoneNumbers = array("12345678901", "12345678902", "12345678903");
+
+// 请确保签名和模板审核通过
+$sender->sendSms("86", $phoneNumbers, "验证码 1234");
+
 ?>
--- demo/SmsMultiDemo.py
+++ demo/SmsMultiDemo.py
@@ -1,3 +1,69 @@
 #! /usr/bin/env python2
 # encoding:utf-8
-# 敬请期待
+# python 2.7 测试通过
+# python 3 更换适当的开发库就能使用，在此我们不额外提供
+
+import httplib
+import json
+import hashlib
+import random
+
+class SmsMultiSender:
+    sdkappid = 0
+    appkey = ""
+    # 请根据我们的开发文档适时调整 url
+    url = "https://yun.tim.qq.com/v3/tlssmssvr/sendmultisms2"
+    
+    def __init__(self, sdkappid, appkey):
+        self.sdkappid = sdkappid
+        self.appkey = appkey
+        
+    def sendMsg(self, nationCode, phoneNumbers, content):
+        """国家码，手机号和内容"""
+        sig = self.calculateSig(self.appkey, phoneNumbers)
+        tel = [ ]
+        for phoneNumber in phoneNumbers:
+            telElement = { "nationcode": nationCode, "phone": phoneNumber }
+            tel.append(telElement);
+            
+        data = { }    
+        data["tel"] = tel
+        data["type"] = "0"
+        data["msg"] = content
+        data["sig"] = sig
+        data["extend"] = ""
+        data["ext"] = ""
+
+        print json.dumps(data)
+
+        con = None
+        try:
+            con = httplib.HTTPSConnection('yun.tim.qq.com', timeout=10)
+            body = json.dumps(data)
+            rnd = random.randint(100000, 999999)
+            wholeUrl = '%s?sdkappid=%d&random=%d' % (self.url, self.sdkappid, rnd)
+            con.request('POST', wholeUrl, body)
+            response = con.getresponse()
+            print response.status,response.reason
+            rsp = response.read()
+            print rsp
+        except Exception,e:
+            print e
+        finally:
+            if(con):
+                con.close()
+
+    def calculateSig(self, appkey, phoneNumbers):
+        """计算 sig"""
+        str = appkey + phoneNumbers[0]
+        for i in range(1, len(phoneNumbers)):
+            str = str + "," + phoneNumbers[i]
+        return hashlib.md5(str).hexdigest()
+
+if __name__ == "__main__":
+    # 开放者实际发送短信时请使用申请的 sdkappid 和 appkey
+    sender = SmsMultiSender(1234567890, "1234567890")
+    # 下列手机号码均不存在，请替换成实际存在的
+    phoneNumbers = [ "12345678901", "12345678902", "12345678903" ]
+    # 请确保签名和模板审核通过
+    sender.sendMsg("86", phoneNumbers, "验证码 1234")
