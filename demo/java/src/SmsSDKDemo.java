d78905d20e6631e868d37a4b70dce0c4af89fd77
qcloudsms <qcloudsms@yeah.net>
2017-03-03 15:06:31 +0800

    修复拉取bug，升级拉取短信至v5版本，拉取短信回执以及回复请使用此版本

--- demo/java/src/SmsSDKDemo.java
+++ demo/java/src/SmsSDKDemo.java
@@ -4,29 +4,36 @@ import com.qcloud.sms.SmsMultiSender;
 import com.qcloud.sms.SmsMultiSenderResult;
 import com.qcloud.sms.SmsSingleSender;
 import com.qcloud.sms.SmsSingleSenderResult;
+import com.qcloud.sms.SmsSingleVoiceSender;
+import com.qcloud.sms.SmsSingleVoiceSenderResult;
+import com.qcloud.sms.SmsVoiceUploader;
+import com.qcloud.sms.SmsVoiceUploaderResult;
+import com.qcloud.sms.SmsStatusPuller;
+import com.qcloud.sms.SmsStatusPullCallbackResult;
+import com.qcloud.sms.SmsStatusPullReplyResult;
 
 public class SmsSDKDemo {
     public static void main(String[] args) {
     	try {
-    		// 请根据实际 appid 和 appkey 进行开发，以下只作为演示 sdk 使用
-    		int appid = 123456;
-    		String appkey = "1234567890abcdef1234567890abcdef";
+    		//请根据实际 appid 和 appkey 进行开发，以下只作为演示 sdk 使用
+    		int appid = 123345555;
+    		String appkey = "1123323412asdadaf323fdadssa";
     		
-    		String phoneNumber1 = "12345678901";
+    		String phoneNumber1 = "12345678902";
     		String phoneNumber2 = "12345678902";
     		String phoneNumber3 = "12345678903";
     		int tmplId = 7839;
 
-    		// 初始化单发
+    		 //初始化单发
 	    	SmsSingleSender singleSender = new SmsSingleSender(appid, appkey);
 	    	SmsSingleSenderResult singleSenderResult;
 	
-	    	// 普通单发
-	    	singleSenderResult = singleSender.send(0, "86", phoneNumber2, "测试短信，普通单发，深圳，小明，上学。", "", "");
+	    	 //普通单发
+	    	singleSenderResult = singleSender.send(0, "86", phoneNumber1, "您注册的验证码：1234", "", "");
 	    	System.out.println(singleSenderResult);
 	
-	    	// 指定模板单发
-	    	// 假设短信模板内容为：测试短信，{1}，{2}，{3}，上学。
+	    	 //指定模板单发
+	    	 //假设短信模板 id 为 1，模板内容为：测试短信，{1}，{2}，{3}，上学。
 	    	ArrayList<String> params = new ArrayList<>();
 	    	params.add("指定模板单发");
 	    	params.add("深圳");
@@ -48,13 +55,22 @@ public class SmsSDKDemo {
 	    	System.out.println(multiSenderResult);
 
 	    	// 指定模板群发
-	    	// 假设短信模板内容为：测试短信，{1}，{2}，{3}，上学。
+	    	// 假设短信模板 id 为 1，模板内容为：测试短信，{1}，{2}，{3}，上学。
 	    	params = new ArrayList<>();
 	    	params.add("指定模板群发");
 	    	params.add("深圳");
 	    	params.add("小明");
 	    	multiSenderResult = multiSender.sendWithParam("86", phoneNumbers, tmplId, params, "", "", "");
 	    	System.out.println(multiSenderResult);
+	    	
+    		
+    		//拉取短信回执和回复
+    		SmsStatusPuller pullstatus = new SmsStatusPuller(appid,appkey);
+    		SmsStatusPullCallbackResult callback_result = pullstatus.pullCallback(10);
+    		System.out.println(callback_result);
+    		SmsStatusPullReplyResult reply_result = pullstatus.pullReply(10);
+    		System.out.println(reply_result);
+    		
     	} catch (Exception e) {
 			e.printStackTrace();
 		}
