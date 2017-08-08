9f6bd78468cc0571704489686476c9c172900f8a
qcloudsms <ellenliu@tencent.com>
2017-08-08 15:14:18 +0800

    增加接入指南链接
    增加接入指南链接

--- demo/csharp/SmsSDKDemo.cs
+++ demo/csharp/SmsSDKDemo.cs
@@ -23,6 +23,7 @@ namespace SmsSDK
         static void Main(string[] args)
         {
             // 请根据实际 appid 和 appkey 进行开发，以下只作为演示 sdk 使用
+            // appid,appkey,templId申请方式可参考接入指南 https://www.qcloud.com/document/product/382/3785#5-.E7.9F.AD.E4.BF.A1.E5.86.85.E5.AE.B9.E9.85.8D.E7.BD.AE
             int sdkappid = 123456;
             string appkey = "1234567890abcdef1234567890abcdef";
             string phoneNumber1 = "12345678901";
