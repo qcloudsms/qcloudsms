0a3673449237d3111e5bc9db4e2aff66b63afd08
weijunyi <weijunyi@tencent.com>
2017-08-29 11:14:25 +0800

    优化内部结构添加注释

--- demo/js/Demo.js
+++ demo/js/Demo.js
@@ -3,22 +3,37 @@
 
 var sender = require('./SmsSender');
 
-// 使用时请更换相应的 sdkappid 和 appkey
+// 使用时请更换相应的 sdkappid 和 appkey。
+// 创建sdkappid,appkey,签名模版等请参考接入指南：https://www.qcloud.com/document/product/382/3785
 sender.config.sdkappid = 12345678;
 sender.config.appkey = '1234567890efabc90888665';
 
-sender.singleSmsSend(0, '86', '12345678901', '您已成功收到来自{1}的{2}元转账，请登录钱包进行查看。', '', '', function (ret) {
-    console.log(ret);
+var phoneNumbers = ['15800000000', '18600000000'];
+
+sender.singleSmsSend(0, '86', phoneNumbers[0], '您已成功收到来自小明的1元转账，请登录钱包进行查看。', '', '', function (data) {
+    var ret = JSON.parse(data);
+    if (0 != ret.result) {
+        console.log(ret);
+    }
 });
 
-sender.singleSmsSendWithParam('86', '12345678901', 184, ['小明','1'], '', '', '', function (ret) {
-    console.log(ret);
+sender.singleSmsSendWithParam('86', phoneNumbers[0], 184, ['小明','1'], '', '', '', function (data) {
+    var ret = JSON.parse(data);
+    if (0 != ret.result) {
+        console.log(ret);
+    }
 });
 
-sender.multiSmsSend(0, '86', ['12345678901', '12345678902'], '您已成功收到来自小明的1元转账，请登录钱包进行查看。', '', '', function (ret) {
-    console.log(ret);
+sender.multiSmsSend(0, '86', phoneNumbers, '您已成功收到来自小明的1元转账，请登录钱包进行查看。', '', '', function (data) {
+    var ret = JSON.parse(data);
+    if (0 != ret.result) {
+        console.log(ret);
+    }
 });
 
-sender.multiSmsSendWithParam('86', ['12345678901', '12345678902'], 184, ['小明','1'], '', '', '', function (ret) {
-    console.log(ret);
+sender.multiSmsSendWithParam('86', phoneNumbers, 184, ['小明','1'], '', '', '', function (data) {
+    var ret = JSON.parse(data);
+    if (0 != ret.result) {
+        console.log(ret);
+    }
 });
--- demo/js/SmsSender.js
+++ demo/js/SmsSender.js
@@ -12,9 +12,20 @@ function getSmsSig(rand, curTime, phoneNumbers) {
         phoneNumberStr += (','+phoneNumbers[i]);
     }
 
-    return crypto.createHash('sha256').update('appkey='+config.appkey+'&random='+rand+'&time='+curTime+'&mobile='+phoneNumberStr).digest('hex');
+    // 一定要使用 utf-8 编码
+    return crypto.createHash('sha256').update('appkey='+config.appkey+'&random='+rand+'&time='+curTime+'&mobile='+phoneNumberStr, 'utf-8').digest('hex');
 }
 
+/**
+ * 单发短信接口
+ * @param {number} msgType 短信类型，0 普通短信，1 营销短信，请严格按照相应的类型填写
+ * @param {string} nationCode 国家码，如果中国 86
+ * @param {string} phoneNumber 手机号
+ * @param {string} msg 短信正文，如果需要带签名，签名请使用【】标注
+ * @param {string} extend 扩展字段，如无需要请填空字符串
+ * @param {string} ext 此字段腾讯云后台服务器会按原样在应答中
+ * @param {function} cb 异步结果回调函数
+ */
 function singleSmsSend(msgType, nationCode, phoneNumber, msg, extend, ext, cb) {
 
     var rand = Math.round(Math.random()*99999);
@@ -23,7 +34,7 @@ function singleSmsSend(msgType, nationCode, phoneNumber, msg, extend, ext, cb) {
     var reqObj = {
         tel: {
             nationCode: nationCode+'',
-            mobile: phoneNumber+'',
+            mobile: phoneNumber+''
         },
         type: Number(msgType),
         msg: msg,
@@ -39,8 +50,8 @@ function singleSmsSend(msgType, nationCode, phoneNumber, msg, extend, ext, cb) {
         } else if (!error && response.statusCode != 200) {
             retObj = {
                 result: -1,
-                errmsg: error.toString() + ' http code ' + response.statusCode
-            }
+                errmsg: 'http code ' + response.statusCode
+            };
             cb(JSON.stringify(retObj));
         } else {
             var retObj = {
@@ -56,12 +67,23 @@ function singleSmsSend(msgType, nationCode, phoneNumber, msg, extend, ext, cb) {
         method: 'POST',
         json: false,
         headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify(reqObj)
     }, callback);
 }
 
+/**
+ * 带模板单发短信接口
+ * @param {string} nationCode 国家码，如果中国 86
+ * @param {string} phoneNumber 手机号
+ * @param {number} templId 短信模板参数，如不清楚，请在 https://console.qcloud.com/sms/smsContent 查看
+ * @param {array} params 模板参数数组，元素类型为 {string}，元素个数请不要超过模板参数个数
+ * @param {string} sign 短信签名
+ * @param {string} extend 扩展字段，如无需要请填空字符串
+ * @param {string} ext 此字段腾讯云后台服务器会按原样在应答中
+ * @param {function} cb 异步结果回调函数
+ */
 function singleSmsSendWithParam(nationCode, phoneNumber, templId, params, sign, extend, ext, cb) {
 
     var rand = Math.round(Math.random()*99999);
@@ -70,7 +92,7 @@ function singleSmsSendWithParam(nationCode, phoneNumber, templId, params, sign, 
     var reqObj = {
         tel: {
             nationCode: nationCode+'',
-            mobile: phoneNumber+'',
+            mobile: phoneNumber+''
         },
         sign: sign,
         tpl_id: Number(templId),
@@ -87,8 +109,8 @@ function singleSmsSendWithParam(nationCode, phoneNumber, templId, params, sign, 
         } else if (!error && response.statusCode != 200) {
             retObj = {
                 result: -1,
-                errmsg: error.toString() + ' http code ' + response.statusCode
-            }
+                errmsg: 'http code ' + response.statusCode
+            };
             cb(JSON.stringify(retObj));
         } else {
             var retObj = {
@@ -104,25 +126,35 @@ function singleSmsSendWithParam(nationCode, phoneNumber, templId, params, sign, 
         method: 'POST',
         json: false,
         headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify(reqObj)
     }, callback);
 }
 
+/**
+ * 群发短信接口
+ * @param {number} msgType 短信类型，0 普通短信，1 营销短信，请严格按照相应的类型填写
+ * @param {string} nationCode
+ * @param {array} phoneNumbers 手机号数组，元素类型为 {string}
+ * @param {string} msg 短信正文，如果需要带签名，签名请使用【】标注
+ * @param {string} extend 扩展字段，如无需要请填空字符串
+ * @param {string} ext 此字段腾讯云后台服务器会按原样在应答中，如不需要请填空字符
+ * @param {function} cb 异步结果回调函数
+ */
 function multiSmsSend(msgType, nationCode, phoneNumbers, msg, extend, ext, cb) {
 
     var rand = Math.round(Math.random()*99999);
     var curTime = Math.floor(Date.now()/1000);
 
     var tel = [];
-    for (var i in phoneNumbers) {
+    for (var i = 0; i < phoneNumbers.length; i++) {
         tel.push({ nationcode: nationCode, mobile: phoneNumbers[i] });
     }
 
     var reqObj = {
         tel: tel,
-        type: Number(type),
+        type: Number(msgType),
         msg: msg,
         sig: getSmsSig(rand, curTime, phoneNumbers),
         time: curTime,
@@ -136,7 +168,7 @@ function multiSmsSend(msgType, nationCode, phoneNumbers, msg, extend, ext, cb) {
         } else if (!error && response.statusCode != 200) {
             retObj = {
                 result: -1,
-                errmsg: error.toString() + ' http code ' + response.statusCode
+                errmsg: 'http code ' + response.statusCode
             };
             cb(JSON.stringify(retObj));
         } else {
@@ -153,19 +185,30 @@ function multiSmsSend(msgType, nationCode, phoneNumbers, msg, extend, ext, cb) {
         method: 'POST',
         json: false,
         headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify(reqObj)
     }, callback);
 }
 
+/**
+ * 带模板群发短信接口
+ * @param {string} nationCode 国家码，如果中国 86
+ * @param {array} phoneNumbers 手机号数组，元素类型为 {string}
+ * @param {number} templId 短信模板参数，如不清楚，请在 https://console.qcloud.com/sms/smsContent 查看
+ * @param {array} params 模板参数数组，元素类型为 {string}，元素个数请不要超过模板参数个数
+ * @param {string} sign 短信签名
+ * @param {string} extend 扩展字段，如无需要请填空字符串
+ * @param {string} ext 此字段腾讯云后台服务器会按原样在应答中
+ * @param {function} cb 异步结果回调函数
+ */
 function multiSmsSendWithParam(nationCode, phoneNumbers, templId, params, sign, extend, ext, cb) {
 
     var rand = Math.round(Math.random()*99999);
     var curTime = Math.floor(Date.now()/1000);
 
     var tel = [];
-    for (var i in phoneNumbers) {
+    for (var i = 0; i < phoneNumbers.length; i++) {
         tel.push({ nationcode: nationCode, mobile: phoneNumbers[i] });
     }
 
@@ -186,7 +229,7 @@ function multiSmsSendWithParam(nationCode, phoneNumbers, templId, params, sign, 
         } else if (!error && response.statusCode != 200) {
             retObj = {
                 result: -1,
-                errmsg: error.toString() + ' http code ' + response.statusCode
+                errmsg: 'http code ' + response.statusCode
             };
             cb(JSON.stringify(retObj));
         } else {
@@ -203,7 +246,7 @@ function multiSmsSendWithParam(nationCode, phoneNumbers, templId, params, sign, 
         method: 'POST',
         json: false,
         headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify(reqObj)
     }, callback);
