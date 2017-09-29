8a1813eb2f1c41278a87a96c98fd862c8e4fe6da
dodoru <dodoru@foxmail.com>
2017-09-29 12:09:36 +0800

    没有使用类变量的方法，改为静态函数。

--- demo/python3/Qcloud/Sms/tools.py
+++ demo/python3/Qcloud/Sms/tools.py
@@ -10,10 +10,12 @@ import time
 class SmsSenderUtil:
     """ 工具类定义 """
 
-    def get_random(self):
+    @staticmethod
+    def get_random():
         return random.randint(100000, 999999)
 
-    def get_cur_time(self):
+    @staticmethod
+    def get_cur_time():
         return int(time.time())
 
     def calculate_sig(self, appkey, rnd, cur_time, phone_numbers):
@@ -23,7 +25,8 @@ class SmsSenderUtil:
         return hashlib.sha256("appkey=" + appkey + "&random=" + str(rnd) + "&time=" + str(cur_time)
                               + "&mobile=" + phone_numbers_string).hexdigest()
 
-    def calculate_sig_for_templ_phone_numbers(self, appkey, rnd, cur_time, phone_numbers):
+    @classmethod
+    def calculate_sig_for_templ_phone_numbers(cls, appkey, rnd, cur_time, phone_numbers):
         """ 计算带模板和手机号列表的 sig """
         phone_numbers_string = phone_numbers[0]
         for i in range(1, len(phone_numbers)):
@@ -41,7 +44,8 @@ class SmsSenderUtil:
             tel.append({"nationcode": nation_code, "mobile":phone_number})
         return tel
 
-    def send_post_request(self, host, url, data):
+    @staticmethod
+    def send_post_request(host, url, data):
         con = None
         try:
             con = http.client.HTTPSConnection(host)
